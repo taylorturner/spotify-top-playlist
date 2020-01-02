92d924c8c014f848312c10e1d3eecd50ace523f0
Taylor Turner <taylorturner244@gmail.com>
2020-01-01 19:34:26 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Spotify Top Hits Ongoing Playlist
+This app uses https://github.com/plamere/spotipy to interact with Spotify API.
+
+This app will parse the Spotify Top Hits playlist and add those songs to a playlist. The app will add newly added songs to the top of this playlist.
+
+## Running app
+Setup ENV
+```
+export SPOTIPY_CLIENT_ID='your-spotify-client-id'
+export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
+export SPOTIPY_REDIRECT_URI='your-app-redirect-url' (http://localhost/)
+```
+See https://spotipy.readthedocs.io/en/latest/#authorization-code-flow for more information
+
+## Todos:
+- Have app pull existing playlist IDs, delete playlist, create new playlist, add newest playlist songs, then add older songs to new playlist. This will ensure that the newest songs will always be at the top of the playlist.
+- Create setup script to export variables, create playlists (above), etc.
+- At the end of each month have Python create a Top Hits Playlist for that Month, at the end of the year combine that into a Yearly Top Hit Playlist.
+- Set a max song limit to the 'main' playlist, default maybe at 200?
---
+++ app.py
@@ -0,0 +1,47 @@
+import sys
+import json
+import pprint
+import spotipy
+import spotipy.util as util
+
+scope = 'playlist-read-private playlist-modify-public'
+top_hits_id = '37i9dQZF1DXcBWIGoYBM5M'
+playlist_id = '5QZxZRZlOBJuVDRky3Uqh6'
+
+
+def get_tracks(username, playlist):
+    results = spotify.user_playlist(username, playlist, fields="tracks")
+    tracks = results['tracks']
+    track_ids = []
+
+    for item in tracks['items']:
+        track = item['track']
+        track_ids.append(track['id'])
+        # track_name = track['name']
+        # track_artist = track['artists'][0]['name']
+        # print(f"{track_name} By: {track_artist} ID: {track_id}")
+    return track_ids
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Usage: {0} username".format(sys.argv[0]))
+        sys.exit()
+
+    token = util.prompt_for_user_token(username, scope)
+    spotify = spotipy.Spotify(auth=token)
+
+    if token:
+        new_tracks = get_tracks(username, top_hits_id)
+        existing_tracks = get_tracks(username, playlist_id)
+        tracks_to_add = []
+        for track in new_tracks:
+            if track not in existing_tracks:
+                tracks_to_add.append(track)
+        # print(tracks_to_add)
+        results = spotify.user_playlist_add_tracks(username, playlist_id, tracks_to_add)
+        pprint.pprint(results)
+    else:
+        print("Can't get token for", username)
